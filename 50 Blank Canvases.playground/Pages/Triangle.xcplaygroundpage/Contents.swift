c578a6dd0c210d8165f5fdfa7052f0482b21c42d
lcs-tdauletov <32198299+lcs-tdauletov@users.noreply.github.com>
2017-11-16 09:19:55 -0500

    Okay, I've got a basic triangle drawn using the turtle metaphor.

--- 50 Blank Canvases.playground/Pages/Triangle.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Triangle.xcplaygroundpage/Contents.swift
@@ -22,8 +22,35 @@ let canvas = Canvas(width: 400, height: 300)
  
  Use whitespace and comments as appropriate.
  */
-// Replace this comment with your first comment – what is the goal of the code you're about to write?
+// Draw a triangle using a metaphor of a "turtle"
 
+// Step 1: move to the starting point
+
+canvas.translate(byX: 75, byY: 100)
+
+canvas.drawAxes()
+canvas.defaultLineWidth = 10
+
+// Step 2: Draw a side and rotate
+
+canvas.drawLine(fromX: 0, fromY: 0, toX: 200, toY: 0)
+canvas.translate(byX: 200, byY: 0)
+canvas.rotate(by: 120)
+
+
+// Step 3: Draw a side and rotate
+
+canvas.drawAxes()
+canvas.drawLine(fromX: 0, fromY: 0, toX: 200, toY: 0)
+canvas.translate(byX: 200, byY: 0)
+canvas.rotate(by: 120)
+
+// Step 4: Draw a side and rotate
+
+canvas.drawAxes()
+canvas.drawLine(fromX: 0, fromY: 0, toX: 200, toY: 0)
+canvas.translate(byX: 200, byY: 0)
+canvas.rotate(by: 120)
 
 
 /*:
